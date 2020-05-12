e9ecee441c443f0f0881e910d36db7d4bd6caf97
sylvanic <christhedankm8@gmail.com>
2020-05-12 14:43:21 +0200

    pickup integrated, drop area as well

---
+++ SpaceJunkGame/Assets/Scripts/Pickable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerStats>().payload < other.GetComponent<PlayerStats>().maxPayload && other.tag == "Player")
+        {
+            other.GetComponent<PlayerStats>().payload++;
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ SpaceJunkGame/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public int payload = 0;
+    public int maxPayload = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Payload: " + payload + "/10");
+    }
+}
