a05128b58565b3eb811be26d526e7fcbb2b10d63
RadinaAgaeva <biohazardsteampunk666@gmail.com>
2020-05-12 17:18:41 +0200

    g
    gg

---
+++ SpaceJunkGame/Assets/Scripts/LaserCooldown_UI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaserCooldown_UI : MonoBehaviour
+{
+    public Image imageCooldown;
+    public float cooldown = 5;
+    bool isCooldown;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            isCooldown = true;
+        }
+        if (isCooldown)
+        {
+            imageCooldown.fillAmount += 1 / cooldown * Time.deltaTime;
+
+            if (imageCooldown.fillAmount >= 1)
+            {
+                imageCooldown.fillAmount = 0;
+                isCooldown = false;
+            }
+        }
+    
+
+    }
+}
