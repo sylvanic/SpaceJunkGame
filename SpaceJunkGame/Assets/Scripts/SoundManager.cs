866e1aeccc3036afaaf2cb6a0e4c6380749f3792
rixphae <rixphae@outlook.com>
2020-05-13 16:59:56 +0200

    added: slightly fixed the issues for the cutscenes

---
+++ SpaceJunkGame/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    //Sound Effects 
+    public AudioSource PickUp;
+    public AudioSource Drop;
+    public AudioSource Shoot;
+
+}
