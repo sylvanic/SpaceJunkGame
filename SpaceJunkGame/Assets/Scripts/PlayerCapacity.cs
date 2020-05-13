40446ed5874e9ca5407eae97d5859bba7b92b0d0
RadinaAgaeva <biohazardsteampunk666@gmail.com>
2020-05-13 22:06:22 +0200

    funcionality to bars added
    they don't work properly

---
+++ SpaceJunkGame/Assets/Scripts/PlayerCapacity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCapacity : MonoBehaviour
+{
+    public float capacity;
+    public Slider slider;
+    public Text text;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        slider.value = capacity;
+        text.text = "Capacity:" + capacity;
+    }
+    void OnCollisionEnter(Collision obj)
+    {
+        if (obj.gameObject.tag == "Trash")
+            capacity = capacity + 1f;
+    }
+}
---
+++ SpaceJunkGame/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health;
+    public Slider slider;
+    public Text text;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = health;
+        text.text = "HP:" + health;
+    }
+    void OnCollisionEnter(Collision obj)
+    {
+        if (obj.gameObject.tag == "Enemy")
+            health = health - 10f;
+    }
+}
