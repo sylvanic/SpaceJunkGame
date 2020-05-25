d3e7a45b1f89dad06aba097ac183ee878b166140
Roz <rixphae@outlook.com>
2020-05-25 11:48:35 +0200

    stuff

--- SpaceJunkGame/Assets/Scripts/MainMenu.cs
+++ SpaceJunkGame/Assets/Scripts/MainMenu.cs
@@ -2,17 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using UnityEditor;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame ()
+
+    [Header("Player Data")]
+    [Space]
+    public string playerName;
+
+    [Space]
+    public TMP_InputField playerNameInputField;
+
+    Button selectedAgeButton;
+    Button selectedGenderButton;
+
+    Color blueColor = Color.blue;
+    Color whiteColor = Color.white;
+
+    private void Start()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
     }
 
-    public void QuitGame ()
+    public void PlayGame()
+    {
+        playerName = playerNameInputField.text;
+
+        if (playerName == "Shauntae")
+        {
+            Debug.Log("welcome to game");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            Debug.Log("fill in your name");
+        }
+
+        Debug.Log(playerName);
+    }
+
+    public void QuitGame()
     {
         Debug.Log("quit");
         Application.Quit();
+    }
+
+    public void AgeGroupSelected(Button button)
+    {
+        if (selectedAgeButton == button )
+        {
+            button.GetComponent<Image>().color = whiteColor;
+            selectedAgeButton = null;
+        }
+        else
+        {
+            if (selectedAgeButton != null)
+            {
+                selectedAgeButton.GetComponent<Image>().color = whiteColor;
+            }
+            button.GetComponent<Image>().color = blueColor;
+            selectedAgeButton = button;
+        }
+    }
+
+    public void GenderSelected(Button button)
+    {
+        if (selectedGenderButton == button)
+        {
+            button.GetComponent<Image>().color = whiteColor;
+            selectedAgeButton = null;
+        }
+        else
+        {
+            if (selectedGenderButton != null)
+            {
+                selectedGenderButton.GetComponent<Image>().color = whiteColor;
+            }
+            button.GetComponent<Image>().color = blueColor;
+            selectedGenderButton = button;
+        }
     }
 }
