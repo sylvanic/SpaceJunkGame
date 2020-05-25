03aa22fb208a92014580a83aa7d5f60c9fa81c1f
UOUGitHub <64587172+UOUGitHub@users.noreply.github.com>
2020-05-25 14:59:16 +0200

    uzay

---
+++ SpaceJunkGame/Assets/Scripts/QuestionYes.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class QuestionYes : MonoBehaviour
+{
+    public void ButtonSceneMove()
+    {
+        //DELETE PRINT CODE AND PLACE SCENE MOVER HERE
+        print("go to level 1");
+    }
+}
---
+++ SpaceJunkGame/Assets/Scripts/TextScroll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextScroll : MonoBehaviour
+{
+    float cont = 0;
+    float scroll;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            scroll = 50;
+            transform.Translate(0, scroll * Time.deltaTime, 0);
+            cont += scroll * Time.deltaTime;
+            print(cont);
+
+        }
+        else
+        {
+            scroll = 4;
+            transform.Translate(0, scroll * Time.deltaTime, 0);
+            cont += scroll * Time.deltaTime;
+            print(cont);
+        }
+
+        if (cont >= 300)
+        {
+            cont = 0;
+            //PLACE NEXT SCENE HERE: LEVEL 1 SCENE
+            
+        }
+    }
+}
