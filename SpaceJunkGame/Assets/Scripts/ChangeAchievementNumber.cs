e43d57e9d420cb4f9e8f43c7005ae44302aa1f8f
UOUGitHub <64587172+UOUGitHub@users.noreply.github.com>
2020-05-28 15:19:56 +0200

    3

---
+++ SpaceJunkGame/Assets/Scripts/ChangeAchievementNumber.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeAchievementNumber : MonoBehaviour
+{
+    public Text achievements;
+    public int Achievements = 5;
+
+    void Update()
+    {
+        achievements.text = Achievements.ToString();
+    }
+}
