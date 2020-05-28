a670002264e4c5b7c0a61e1bb5dbb87c45388bea
UOUGitHub <64587172+UOUGitHub@users.noreply.github.com>
2020-05-28 15:06:38 +0200

    1

---
+++ SpaceJunkGame/Assets/Scripts/DebrisSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebrisSpawn : MonoBehaviour
+{
+    float cont = 0f;
+    float scroll;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            scroll = 50;
+            cont += scroll * Time.deltaTime;
+        }
+        else
+        {
+            scroll = 4;
+            cont += scroll * Time.deltaTime;
+        }
+
+        if (cont >= 150) //edit to when scrolling text says how humanity didnt fix the space debris problem yet
+        {
+            print("SPAWN SPACE DEBRIS AROUND PLANET");
+        }
+    }
+}
---
+++ SpaceJunkGame/Assets/Scripts/TextScrollCutscene3.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextScrollCutscene3 : MonoBehaviour
+{
+    float cont = 0;
+    float scroll;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            scroll = 50;
+            transform.Translate(0, scroll * Time.deltaTime, 0);
+            cont += scroll * Time.deltaTime;
+
+        }
+        else
+        {
+            scroll = 4;
+            transform.Translate(0, scroll * Time.deltaTime, 0);
+            cont += scroll * Time.deltaTime;
+        }
+
+        if (cont >= 300)
+        {
+            cont = 0;
+            //PLACE NEXT SCENE HERE: LEVEL MAIN MENU SCENE
+
+        }
+    }
+}
