cc40c080de62367ea69faf480d10758200ae3e92
Roz <rixphae@outlook.com>
2020-06-02 22:11:10 +0200

    LevelMenu + Upgrade Screen
    Added select level menu and upgrade screen

---
+++ SpaceJunkGame/Assets/Scripts/SelectLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectLevel : MonoBehaviour
+{
+    public Button Level1, Level2, Level3;
+
+    public void ChosenLevel()
+    {
+        Level1.onClick.AddListener(SelectLevel1);
+
+        Level2.onClick.AddListener(SelectLevel2);
+
+        Level3.onClick.AddListener(SelectLevel3);
+
+    }
+    
+    private void SelectLevel1()
+    {
+        SceneManager.LoadScene("GameTest");
+    }
+
+    private void SelectLevel2()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+
+    private void SelectLevel3()
+    {
+        SceneManager.LoadScene("TutorialF");
+    }
+}
