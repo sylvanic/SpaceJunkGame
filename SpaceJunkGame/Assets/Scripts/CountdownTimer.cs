5ddec9f6ca8af1e4a454d09773aabc7734d8e3ce
RadinaAgaeva <biohazardsteampunk666@gmail.com>
2020-06-04 17:51:50 +0200

    changes in the script

--- SpaceJunkGame/Assets/Scripts/CountdownTimer.cs
+++ SpaceJunkGame/Assets/Scripts/CountdownTimer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using UnityEditor.Build.Player;
 
 public class CountdownTimer : MonoBehaviour
 {
