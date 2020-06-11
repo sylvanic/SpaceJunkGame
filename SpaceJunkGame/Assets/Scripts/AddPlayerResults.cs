88d3c07b1993f96105fb5a9665baa88df4368ded
jonas_virsila <jonas.virsila855@gmail.com>
2020-06-11 19:52:14 +0200

    Updated Leaderboard Scripts

---
+++ SpaceJunkGame/Assets/Scripts/AddPlayerResults.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class AddPlayerResults : MonoBehaviour
+{ 
+    void Awake()
+    {
+        //AddPlayerScores(ScoreDisplay.score, DisplayPlayerName.playerUsername);        
+    }
+
+    private void AddPlayerScores(int score, string name)
+    {
+        PlayerScore playerScore = new PlayerScore { score = score, name = name };
+
+        string jsonString = PlayerPrefs.GetString("leaderboard");
+        Scores highscores = JsonUtility.FromJson<Scores>(jsonString);
+
+        if (highscores == null)
+        {
+            // There's no stored table, initialize
+            highscores = new Scores()
+            {
+                scoreList = new List<PlayerScore>()
+            };
+        }
+
+        highscores.scoreList.Add(playerScore);
+
+        string json = JsonUtility.ToJson(highscores);
+        PlayerPrefs.SetString("leaderboard", json);
+        PlayerPrefs.Save();
+        Debug.Log(PlayerPrefs.GetString("leaderboard"));
+    }
+
+    private class Scores
+    {
+        public List<PlayerScore> scoreList;
+    }
+
+    [System.Serializable]
+    private class PlayerScore
+    {
+        public int score;
+        public string name;
+    }
+}
