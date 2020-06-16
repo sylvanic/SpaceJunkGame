762b30dccba5e96c72084525dc89ab789259a72f
Roz <rixphae@outlook.com>
2020-06-16 14:24:55 +0200

    objectives + challenges translated

--- SpaceJunkGame/Assets/Scripts/LanguageSwitchScripts/LangSwitchBool.cs
+++ SpaceJunkGame/Assets/Scripts/LanguageSwitchScripts/LangSwitchBool.cs
@@ -3,9 +3,7 @@
 public class LangSwitchBool : MonoBehaviour
 {
     public static bool englishTxt = false;
-    public static string hitTaskTxt;
-    public static string pickUpTaskTxt;
-    public static string scoreTaskTxt;
+
 
     public void Dutch()
     {
@@ -15,21 +13,5 @@ public class LangSwitchBool : MonoBehaviour
     public void English()
     {
         englishTxt = true;
-    }
-
-    private void Awake()
-    {
-        if (englishTxt)
-        {
-            hitTaskTxt = "Get hit by an asteroid {0}/{1} times";
-            pickUpTaskTxt = "Collected {0}/{1} junk";
-            scoreTaskTxt = "Obtain a score of {0}/{1} points";
-        }
-        else
-        {
-            hitTaskTxt = "Word door een asteroid {0}/{1} keer geraakt";
-            pickUpTaskTxt = "Verzamel {0}/{1} afval";
-            scoreTaskTxt = "Verzamel {0}/{1} punten";
-        }
     }
 }
