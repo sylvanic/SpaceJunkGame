7da327988f6dd5052c12d2fec8e039c6e1e563c6
Roz <rixphae@outlook.com>
2020-06-16 12:56:50 +0200

    translated: endscreen + ratescreen

---
+++ SpaceJunkGame/Assets/Scripts/LanguageSwitchScripts/LanguageSwitchEndScoreboard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LanguageSwitchEndScoreboard : MonoBehaviour
+{
+    public TextMeshProUGUI ESTitle;
+    public TextMeshProUGUI Level1;
+    public TextMeshProUGUI Level2;
+    public TextMeshProUGUI Level3;
+    public TextMeshProUGUI TotalScore;
+    public TextMeshProUGUI Replay;
+    public TextMeshProUGUI Menu;
+    public TextMeshProUGUI Rate;
+
+    private void Awake()
+    {
+        if (LangSwitchBool.englishTxt)
+        {
+            ESTitle.text = "GAME COMPLETED!";
+            Level1.text = "LEVEL 1:";
+            Level2.text = "LEVEL 2:";
+            Level3.text = "LEVEL 3:";
+            TotalScore.text = "TOTAL SCORE";
+            Replay.text = "REPLAY";
+            Menu.text = "MENU";
+            Rate.text = "RATE GAME";
+        }
+        else
+        {
+            ESTitle.text = "SPEL AFGEROND!";
+            Level1.text = "NIVEAU 1:";
+            Level2.text = "NIVEAU 2:";
+            Level3.text = "NIVEAU 3:";
+            TotalScore.text = "TOTALE PUNTEN";
+            Replay.text = "OPNIEUW";
+            Menu.text = "MENU";
+            Rate.text = "BEOORDEEL SPEL";
+        }
+    }
+
+
+}
---
+++ SpaceJunkGame/Assets/Scripts/LanguageSwitchScripts/LanguageSwitchRateGame.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class LanguageSwitchRateGame : MonoBehaviour
+{
+    public TextMeshProUGUI P1Title;
+    public TextMeshProUGUI P1Enjoy;
+    public TextMeshProUGUI P1Back;
+    public TextMeshProUGUI P1Rate;
+    [Space]
+    public TextMeshProUGUI F1Title;
+    public TextMeshProUGUI F1Wrong;
+    public TMP_Dropdown F1DropDown;
+    public TextMeshProUGUI F1Next;
+    [Space]
+    public TextMeshProUGUI P2Title;
+    public TextMeshProUGUI P2Learning;
+    public TMP_Dropdown F2DropDown;
+    public TextMeshProUGUI P2Finish;
+    [Space]
+    public TextMeshProUGUI F2Title;
+    public TextMeshProUGUI F2Wrong;
+
+    public TextMeshProUGUI F2Finish;
+    [Space]
+    public TextMeshProUGUI Thanks;
+    public TextMeshProUGUI Back;
+    public TextMeshProUGUI Menu;
+
+
+    private void Awake()
+    {
+        if (LangSwitchBool.englishTxt)
+        {
+            P1Title.text = "Rate our Game!";
+            P1Enjoy.text = "Did you enjoy it?";
+            P1Back.text = "Back";
+            P1Rate.text = "Rate";
+
+            F1Title.text = "Rate our Game!";
+            F1Wrong.text = "What went wrong?";
+
+            F1Next.text = "Next";
+
+            P2Title.text = "Rate our Game!";
+            P2Learning.text = "Was it a good learning experience?";
+            P2Finish.text = "Finish";
+
+            F2Title.text = "Rate our Game!";
+            F2Wrong.text = "What went wrong?";
+            F2Finish.text = "Finish";
+
+            Thanks.text = "Thank you for your\n feedback!";
+            Back.text = "Back";
+            Menu.text = "Menu";
+
+        }
+        else
+        {
+            P1Title.text = "Beoordeel ons Spel!";
+            P1Enjoy.text = "Vond je het leuk?";
+            P1Back.text = "Terug";
+            P1Rate.text = "Beoordel";
+
+            F1Title.text = "Beoordeel ons Spel!";
+            F1Wrong.text = "Wat ging er mis?";
+            F1Next.text = "Volgende";
+
+            P2Title.text = "Beoordeel ons Spel!";
+            P2Learning.text = "Was het een goede leerervaring";
+            P2Finish.text = "Einde";
+
+            F2Title.text = "Beoordeel ons Spel!";
+            F2Wrong.text = "Wat ging er mis?";
+            F2Finish.text = "Einde";
+
+            Thanks.text = "Bedankt voor uw\n feedback!";
+            Back.text = "Terug";
+            Menu.text = "Menu";
+
+        }
+    }
+
+
+}
