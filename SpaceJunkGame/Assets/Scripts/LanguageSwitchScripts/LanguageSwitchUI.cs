ee73281340aa7d4c7b91d48ecad29f6c4ecefe9a
UOUGitHub <64587172+UOUGitHub@users.noreply.github.com>
2020-06-16 13:43:54 +0200

    ui canvas

---
+++ SpaceJunkGame/Assets/Scripts/LanguageSwitchScripts/LanguageSwitchUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageSwitchUI : MonoBehaviour
+{
+    public GameObject timer;
+    public GameObject storageCapacity;
+    public GameObject objectives;
+    public GameObject challenges;
+    public GameObject gameTitle;
+    public GameObject quit;
+    public GameObject confirmationQuit;
+    public GameObject confirmationWarning;
+    public GameObject confirmationYes;
+    public GameObject confirmationNo;
+
+}
