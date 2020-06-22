ea8b76bafd055b6a4e038ccc24a38cbf05b304ef
UOUGitHub <64587172+UOUGitHub@users.noreply.github.com>
2020-06-22 12:12:00 +0200

    assadasd

--- SpaceJunkGame/Assets/Scripts/HelpMenu.cs
+++ SpaceJunkGame/Assets/Scripts/HelpMenu.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class HelpMenu : MonoBehaviour
 {
+    public GameObject HintButton;
+    public GameObject MenuPage1;
+    public GameObject MenuPage2;
+    public GameObject MenuPage3;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,38 @@ public class HelpMenu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void CloseMenu()
+    {
+        HintButton.SetActive(true);
+    }
+
+    public void OpenMenu()
+    {
+        HintButton.SetActive(false);
+        MenuPage1.SetActive(true);
+    }
+
+    public void GoToPage1fromPage2()
+    {
+        MenuPage2.SetActive(false);
+        MenuPage1.SetActive(true);
+    }
+
+    public void GoToPage2fromPage1()
+    {
+        MenuPage1.SetActive(false);
+        MenuPage2.SetActive(true);
+    }
+
+    public void GoToPage3fromPage2()
+    {
+
+    }
+
+    public void GoToPage2fromPage3()
+    {
+
     }
 }
