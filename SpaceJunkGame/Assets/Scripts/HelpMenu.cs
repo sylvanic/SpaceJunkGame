0fc26ccbbc212f6278791e340ff411edead798e2
UOUGitHub <64587172+UOUGitHub@users.noreply.github.com>
2020-06-22 13:12:51 +0200

    fixed cutscenes

--- SpaceJunkGame/Assets/Scripts/HelpMenu.cs
+++ SpaceJunkGame/Assets/Scripts/HelpMenu.cs
@@ -5,51 +5,46 @@ using UnityEngine;
 public class HelpMenu : MonoBehaviour
 {
     public GameObject HintButton;
+    public GameObject exitButton;
     public GameObject MenuPage1;
     public GameObject MenuPage2;
     public GameObject MenuPage3;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void CloseMenu()
     {
-        HintButton.SetActive(true);
+        exitButton.SetActive(false);
+        MenuPage1.SetActive(false);
+        MenuPage2.SetActive(false);
+        MenuPage3.SetActive(false);
     }
 
     public void OpenMenu()
     {
-        HintButton.SetActive(false);
+        exitButton.SetActive(true);
         MenuPage1.SetActive(true);
     }
 
     public void GoToPage1fromPage2()
     {
-        MenuPage2.SetActive(false);
-        MenuPage1.SetActive(true);
+        MenuPage2.SetActive(true);
+        MenuPage1.SetActive(false);
     }
 
     public void GoToPage2fromPage1()
     {
-        MenuPage1.SetActive(false);
-        MenuPage2.SetActive(true);
+        MenuPage2.SetActive(false);
+        MenuPage1.SetActive(true);
     }
 
     public void GoToPage3fromPage2()
     {
-
+        MenuPage3.SetActive(false);
+        MenuPage2.SetActive(true);
     }
 
     public void GoToPage2fromPage3()
     {
-
+        MenuPage3.SetActive(true);
+        MenuPage2.SetActive(false);
     }
 }
