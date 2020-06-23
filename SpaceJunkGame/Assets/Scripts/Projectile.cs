65d6d500c2e3e3890403c8bdfcadf31a2c2b4691
sylvanic <christhedankm8@gmail.com>
2020-06-23 11:19:22 +0200

    expl  prefab script

--- SpaceJunkGame/Assets/Scripts/Projectile.cs
+++ SpaceJunkGame/Assets/Scripts/Projectile.cs
@@ -6,7 +6,7 @@ public class Projectile : MonoBehaviour
 {
     protected float speed = 10;
 
-    public GameObject explosionShootPrefab;
+    protected GameObject explosionShootPrefab;
 
 
     private void Start()
