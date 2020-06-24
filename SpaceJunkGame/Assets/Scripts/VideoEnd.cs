155513c428b47bcaaac70f5cf2c46816441a2449
UOUGitHub <64587172+UOUGitHub@users.noreply.github.com>
2020-06-24 11:11:17 +0200

    new intros added

---
+++ SpaceJunkGame/Assets/Scripts/VideoEnd.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoEnd : MonoBehaviour
+{
+    public float time = 55f;
+
+    private SceneManagement sceneManager;
+
+    private void Awake()
+    {
+        sceneManager = GetComponent<SceneManagement>();
+    }
+
+    void Update()
+    {
+        time -= 1 * Time.deltaTime;
+
+        if (time <= 0)
+        {
+            sceneManager.IntroFamiliarityYes();
+        }
+    }
+}
